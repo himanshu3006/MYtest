0d95ebbc0c88c8fc97a69a5159c1011d3a742407
himanshu3006 <32892920+himanshu3006@users.noreply.github.com>
2017-11-28 16:06:37 +0530

    Update c.c

--- c.c
+++ c.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main() {
-    int n1, n2, i, gcd;
+    int n1, n3, i, gcd;
     printf("Enter two integers: ");
-    scanf("%d %d", &n1, &n2);
-    for(i=1; i <= n1 && i <= n2; ++i)
+    scanf("%d %d", &n1, &n3);
+    for(i=1; i <= n1 && i <= n3; ++i)
     {
         // Checks if i is factor of both integers
-        if(n1%i==0 && n2%i==0)
+        if(n1%i==0 && n3%i==0)
             gcd = i;
     }
-    printf("G.C.D of %d and %d is %d", n1, n2, gcd);
+    printf("G.C.D of %d and %d is %d", n1, n3, gcd);
     return 0;
 }
