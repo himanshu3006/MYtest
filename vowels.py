0d90334039a2c469fa55185d74c9f428e6003f5b
himanshu3006 <32892920+himanshu3006@users.noreply.github.com>
2017-12-09 15:32:10 +0530

    Delete vowels.py

--- vowels.py
+++
@@ -1,7 +0,0 @@
-String=inputstring("enter a string:")
-vowels=0
-for i in String:
-        if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U' ):
-             vowels=vowels + 1
-print("Number of vowels are:")
-print(vowels)
