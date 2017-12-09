c635c50b2f68be4964d3f4c6d5ec8dd3ea6006d1
himanshu3006 <32892920+himanshu3006@users.noreply.github.com>
2017-12-09 15:06:18 +0530

    commit_04
    input updated with inputstring

--- vowels.py
+++ vowels.py
@@ -1,4 +1,4 @@
-String=input("enter a string:")
+String=inputstring("enter a string:")
 vowels=0
 for i in String:
         if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U' ):
