091f99122e5ab162640bbd5a91c7ad3655b0e1cd
himanshu3006 <32892920+himanshu3006@users.noreply.github.com>
2017-12-09 12:47:19 +0530

    commit

---
+++ vowels.py
@@ -0,0 +1,7 @@
+String=input("enter a string:")
+vowels=0
+for i in String:
+        if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U' ):
+             vowels=vowels + 1
+print("Number of vowels are:")
+print(vowels)
